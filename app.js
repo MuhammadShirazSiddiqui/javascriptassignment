d01e5865349305da70b7835430ecc26ccb0ba0ca
Muhammad Shiraz Siddiqui <shirazsiddiqui44@gmail.com>
2020-06-07 16:39:20 +0500

    1st commit

---
+++ app.js
@@ -0,0 +1,388 @@
+// Chapter # 1 => Question # 1
+// alert("Welcome to Javascript Alert Page!");
+
+
+// Chapter # 1 => Question # 2
+// alert("Error! Please enter a valid password.");
+
+
+// Chapter # 1 => Question # 3
+// alert("Welcome to JS Land... \n\Happy Coding.");
+
+
+// Chapter # 1 => Question # 4
+// alert("Welcome to JS Land.");
+// alert("Happy Coding.");
+
+
+// Chapter # 1 => Question # 5
+// alert("Hello... I can run JS through my web browser's console.");
+
+
+// Chapter # 1 => Question # 6                                              //not done
+// alert("Welcome to Hikers Agency");                                       
+
+
+// Chapter # 1 => Question # 7                                              //not done
+// alert("Hello... I can run JS through my web browser's console.");        
+
+
+// Chapter # 2 => Question # 1
+// var username;
+
+
+// Chapter # 2 => Question # 2
+// var myName = "Muhammad Shiraz Siddiqui";
+
+
+// Chapter # 2 => Question # 3
+// var message;
+// message = "Hello World";
+// alert(message);
+
+
+// Chapter # 2 => Question # 4
+// var name = "John Doe";
+// var age = 15;
+// var title = "Certified Mobile / Application Development";
+// alert(name);
+// alert(age+" years old");
+// alert(title);
+
+// Chapter # 2 => Question # 5                                              //not done                    
+// var pizza = "pizza";
+// for(var i = 0; i <)                                                      
+
+
+// Chapter # 2 => Question # 6
+// var email = "example@example.com";
+// alert("My email address is "+email);
+
+
+// Chapter # 2 => Question # 7
+// var book = "A smarter way to learn JavaScript.";
+// alert("I am trying to learn from the Book "+book);
+
+
+// Chapter # 2 => Question # 8
+// document.write("Yah! I can write HTML content through JavaScript");
+
+
+// Chapter # 2 => Question # 9
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(design);
+
+
+// Chapter # 3 => Question # 1
+// var age = 24;
+// alert("I am "+age+" years old");
+
+
+// Chapter # 3 => Question # 2
+// var visit = 1;
+
+
+// Chapter # 3 => Question # 3                                              //not done
+// var birthyear = 1995;
+// document.write("My Birthyear is "+birthyear+"<br>");
+// document.write("Date type of my declared variable is number");           
+
+
+// Chapter # 3 => Question # 4
+// var visitor_name= "John Doe";
+// visitor_name = visitor_name.bold();
+// var product_title = "T-shirt";
+// product_title = product_title.bold();
+// var quantity = 5;
+// quantity = quantity.toString();
+// quantity = quantity.bold();
+// document.write(visitor_name+" ordered "+quantity+" "+product_title+"(s) on XYZ Clothing Store.");
+
+
+// Chapter # 4 => Question # 1
+// var number, age, integer;
+
+
+// Chapter # 4 => Question # 2
+// var number;
+// var $dollar;
+// var _hyphen;
+// var _variable2ndName;
+// var $my_1stVariable;
+
+// var @name;
+// var -age;
+// var ?gender;
+// var !class;
+// var &year;
+
+
+// Chapter # 4 => Question # 3
+// document.write(`<h1 style="font-weight: bold">Rules for naming JS variables</h1> <br><br><br>`);
+// document.write(`Variable name can only contain, number, $ and _. For example: $my_1stVariable <br>`);
+// document.write(`Variable must begin with a letter, $ or _. For example: $name, _name or name <br>`);
+// document.write(`Variable names are case sensitive <br>`);
+// document.write(`Variable names should not be JS keywords <br>`);
+
+
+// Chapter # 5 => Question # 1
+// var num1 = parseInt(prompt());
+// var num2 = parseInt(prompt());
+// var add = num1+num2;
+// document.write(`Sum of ${num1} and ${num2} is ${add}`);
+
+
+// Chapter # 5 => Question # 2
+// var subtract = num1-num2;
+// document.write(`Subtraction of ${num1} and ${num2} is ${subtract}`);
+// var multiplication = num1*num2;
+// document.write(`Multiplication of ${num1} and ${num2} is ${multiplication}`);
+// var divide = num1/num2;
+// document.write(`Division of ${num1} and ${num2} is ${divide}`);
+// var modulus = num1%num2;
+// document.write(`Remainder of ${num1} and ${num2} is ${modulus}`);
+
+
+// Chapter # 5 => Question # 3
+// var num;
+// document.write("Value after variable declaration is "+num+" <br> ");
+// num = parseInt(prompt());
+// document.write("Initial Value: "+num+" <br> ");
+// num++;
+// document.write("Value after Increment is: "+num+" <br> ");
+// num = num+7;
+// document.write("Value after Addition is: "+num+" <br> ");
+// num--;
+// document.write("Value after Decrement is: "+num+" <br> ");
+// num = num%3;
+// document.write("The Remainder is: "+num+" <br> ");
+
+
+// Chapter # 5 => Question # 4
+// var movie_ticket = 600;
+// var num = parseInt(prompt());
+// var total = movie_ticket*num;
+// total = total.toString();
+// total = total.bold();
+// document.write(`Total cost of ${num} tickets to a movie is ${total}<b>PKR</b>`)
+
+
+// Chapter # 5 => Question # 5
+// var table = parseInt(prompt());
+// document.write(`Table of ${table}<br>`);
+// for(var i = 1; i <= 10; i++){
+//     document.write(`${table} x ${i} = ${i*table} <br>`);
+// }
+
+
+// Chapter # 5 => Question # 6
+// var c = parseInt(prompt());
+// var fahrenheit = (c*9/5)+32;
+// document.write(`${c}'C is ${fahrenheit}'F <br>`);
+
+// var f = parseInt(prompt());
+// var celsius = (f-32)*5/9;
+// document.write(`${f}'C is ${celsius}'F`);
+
+
+// Chapter # 5 => Question # 7
+// var item1 = 650;
+// var item2 = 100;
+// var quantity1 = 3;
+// var quantity2 = 7;
+// var shippingCharges = 100;
+// var total = item1*quantity1 + item2*quantity2 + shippingCharges;
+
+// document.write(`<h1 style="font-weight: bold">Shopping Cart</h1> <br><br><br>`);
+// document.write(`Price of item 1 is ${item1}<br>`);
+// document.write(`Quantity of item 1 is ${quantity1}<br>`);
+// document.write(`Price of item 2 is ${item1}<br>`);
+// document.write(`Quantity of item 2 is ${quantity2}<br>`);
+// document.write(`Shipping Charges ${shippingCharges}<br><br>`);
+// document.write(`Total cost of your order is ${total}<br>`);
+
+
+// Chapter # 5 => Question # 8
+// var totalMarks= 980;
+// var marksObtained = 804;
+// var percentage = (marksObtained / totalMarks) * 100;
+
+// document.write(`<h1 style="font-weight: bold">Marks Sheet</h1><br><br>`);
+// document.write(`Total marks: ${totalMarks}<br>`);
+// document.write(`Marks obtained: ${marksObtained}<br>`);
+// document.write(`Percentage: ${percentage}%`);
+
+
+// Chapter # 5 => Question # 9
+// var USdollar = 10;
+// var saudiRiyals = 25;
+// var currency = (USdollar*104.80) + (saudiRiyals*28);
+
+// document.write(`<h1 style="font-weight: bold">Currency in PKR</h1><br><br>`);
+// document.write(`Total Currency in PKR: ${currency}`)
+
+
+// Chapter # 5 => Question # 10
+// var num = parseInt(prompt());
+// var calculation = ((num + 5) * 10) / 2;
+// document.write(calculation);
+
+
+// Chapter # 5 => Question # 11
+// var d = new Date();
+// var year = d.getFullYear();
+// var birthYear = parseInt(prompt("Type your Birth Year:"));
+// var age = year - birthYear;
+// var age2 = age - 1;
+
+// document.write(`<h1 style="font-weight: bold">Age Calculator</h1><br><br>`);
+// document.write(`Current Year: ${year} <br>`);
+// document.write(`Birth Year: ${birthYear} <br>`);
+// document.write(`Your Age is : ${age2} or ${age}<br>`);
+
+
+// Chapter # 5 => Question # 12
+// var radius = parseInt(prompt());
+// var circumference = 2 * 3.142 * radius;
+// var area = 3.142 * radius * radius;
+
+// document.write(`<h1 style="font-weight: bold">The Geometrizer</h1><br><br>`);
+// document.write(`Radius of a circle: ${radius} <br>`);
+// document.write(`The circumference is: ${circumference} <br>`);
+// document.write(`The area is: ${area} <br>`);
+
+
+// Chapter # 5 => Question # 13
+// var favSnack = prompt("Favourite Snack");
+// var currentAge = parseInt(prompt("Current Age"));
+// var estimatedAge = parseInt(prompt("Estimated Maximum Age"));
+// var perDay = parseInt(prompt("Amount of snacks per day"));
+// var totalquantity = ((estimatedAge-currentAge)*365)*perDay;
+
+// document.write(`<h1 style="font-weight: bold">The Lifetime Supply Calculator</h1><br><br>`);
+// document.write(`Favourite Snack: ${favSnack} <br>`);
+// document.write(`Current Age: ${currentAge} <br>`);
+// document.write(`Estimated Maximum Age: ${estimatedAge} <br>`);
+// document.write(`Amount of snacks per day: ${perDay} <br>`);
+// document.write(`You will need ${totalquantity} ${favSnack} to last you until the ripe old age of ${estimatedAge}<br>`);
+
+
+// Chapter # 6-9 => Question # 1
+// var a = parseInt(prompt("Type a number"));
+// document.write(`<b>Result:</b> <br>`)
+// document.write(`The value of a is: ${a} <br>`)
+// document.write(`........................................ <br><br>`)
+// document.write(`The value of ++a is: ${++a} <br>`)
+// document.write(`Now the value of a is: ${a} <br><br>`)
+// document.write(`The value of a++ is: ${a++} <br>`)
+// document.write(`Now the value of a is: ${a} <br><br>`)
+// document.write(`The value of --a is: ${--a} <br>`)
+// document.write(`Now the value of a is: ${a} <br><br>`)
+// document.write(`The value of a-- is: ${a--} <br>`)
+// document.write(`Now the value of a is: ${a} <br><br>`)
+
+
+// Chapter # 6-9 => Question # 2
+// var a = 2, b = 1;
+// var result = (((--a) - (--b)) + (++b)) + (b--);
+// document.write(`a is ${a} <br>`);
+// document.write(`b is ${b} <br>`);
+// document.write(`result is ${result} <br>`);
+
+
+// Chapter # 6-9 => Question # 3
+// var name = prompt("Type your name:");
+// alert(`We welcome you ${name}`);
+// document.write(`<h1 style="font-weight: bold"> We welcome you ${name} </h1>`);
+
+
+// Chapter # 6-9 => Question # 4,5                                         // not done
+// var table = parseInt(prompt("Type a number"))
+// if (table === undefined || null)
+// {
+//     for (var i = 1; i <= 10; i++){
+//         document.write(`5 x ${i} = ${5*i} <br>`);
+//     }
+// }else{
+//     for (var i = 1; i <= 10; i++){
+//         document.write(`${table} x ${i} = ${table*i} <br>`);
+//     }
+// }
+
+
+// Chapter # 6-9 => Question # 6                                           // not done
+// var sub1 = prompt("1st Subject:");
+// var sub2 = prompt("2nd Subject:");
+// var sub3 = prompt("3rd Subject:");
+// var totalMarks = 100;
+// var obtained1 = parseInt(prompt(`Marks obtained in ${sub1}:`))
+// var obtained2 = parseInt(prompt(`Marks obtained in ${sub2}:`))
+// var obtained3 = parseInt(prompt(`Marks obtained in ${sub3}:`))
+
+
+// Chapter # 9-11 => Question # 1                                          //case sensitive issue
+// var city = prompt("Enter City Name:");
+// if(city === "karachi"){
+//     document.write(`City of Lights`);
+// } 
+// else if(city === "Lahore"){
+//     document.write(`Welcome to Lahore`);
+// }
+// else if(city === "Islamabad"){
+//     document.write(`Welcome to Islamabad`);
+// }
+// else{
+//     document.write(`I don't know about this city.`)
+// }
+
+
+// Chapter # 9-11 => Question # 2
+// var gender = prompt("Type your Gender:");
+// if(gender === "male"){
+//     document.write(`Good Morning Sir`);
+// } 
+// else if(gender === "female"){
+//     document.write(`Good Morning Ma'am`);
+// }
+
+
+// Chapter # 9-11 => Question # 4                                           //logic confusion
+// var fuel = parseInt(prompt("Remaining Fuel in Litres"));
+// if(fuel > 0.25){
+    
+// } 
+// else{
+//     alert(`Please refill the fuel in your car`);
+//     document.write(`Please refill the fuel in your car`);
+// }
+
+
+// Chapter # 9-11 => Question # 6  
+// var marksObtained1 = parseInt(prompt('Marks obtained in 1st Subject'));
+// var marksObtained2 = parseInt(prompt('Marks obtained in 2nd Subject'));
+// var marksObtained3 = parseInt(prompt('Marks obtained in 3rd Subject'));
+// var totalmarksObtained = marksObtained1 + marksObtained2 + marksObtained3;
+// var percentage = (totalmarksObtained / 300)*100;
+// document.write(`<h1 style="font-weight: bold">Marks Sheet</h1><br><br>`);
+// document.write(`Total Marks: 300 <br>`);
+// document.write(`Marks Obtained: ${totalmarksObtained} <br>`);
+// document.write(`Percentage: ${percentage}% <br>`);
+// if(percentage >= 80){
+//     document.write(`Grade: A-one <br>`);
+//     document.write(`Remarks: Excellent <br>`);
+// }
+// else if(percentage >= 70 ){
+//     document.write(`Grade: A <br>`);
+//     document.write(`Remarks: Good <br>`);
+
+// }
+// else if(percentage >= 60 ){
+//     document.write(`Grade: B <br>`);   
+//     document.write(`Remarks: You need to improve <br>`);
+// }
+// else{
+//     document.write(`Fail <br>`);
+//     document.write(`Remarks: Sorry <br>`);
+// }
+
+
