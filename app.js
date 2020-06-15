94c669e578b42fafeb947209670d2c9893122013
Muhammad Shiraz Siddiqui <shirazsiddiqui44@gmail.com>
2020-06-15 06:01:08 +0500

    3rd changes

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+
 // Chapter # 1 => Question # 1
 // alert("Welcome to Javascript Alert Page!");
+
 
 
 // Chapter # 1 => Question # 2
 // alert("Error! Please enter a valid password.");
 
 
+
 // Chapter # 1 => Question # 3
 // alert("Welcome to JS Land... \n\Happy Coding.");
+
 
 
 // Chapter # 1 => Question # 4
@@ -15,30 +19,37 @@
 // alert("Happy Coding.");
 
 
+
 // Chapter # 1 => Question # 5
 // alert("Hello... I can run JS through my web browser's console.");
+
 
 
 // Chapter # 1 => Question # 6                                              //not done
 // alert("Welcome to Hikers Agency");                                       
 
 
+
 // Chapter # 1 => Question # 7                                              //not done
 // alert("Hello... I can run JS through my web browser's console.");        
+
 
 
 // Chapter # 2 => Question # 1
 // var username;
 
 
+
 // Chapter # 2 => Question # 2
 // var myName = "Muhammad Shiraz Siddiqui";
+
 
 
 // Chapter # 2 => Question # 3
 // var message;
 // message = "Hello World";
 // alert(message);
+
 
 
 // Chapter # 2 => Question # 4
@@ -49,9 +60,12 @@
 // alert(age+" years old");
 // alert(title);
 
+
+
 // Chapter # 2 => Question # 5                                              //not done                    
 // var pizza = "pizza";
 // for(var i = 0; i <)                                                      
+
 
 
 // Chapter # 2 => Question # 6
@@ -59,13 +73,16 @@
 // alert("My email address is "+email);
 
 
+
 // Chapter # 2 => Question # 7
 // var book = "A smarter way to learn JavaScript.";
 // alert("I am trying to learn from the Book "+book);
 
 
+
 // Chapter # 2 => Question # 8
 // document.write("Yah! I can write HTML content through JavaScript");
+
 
 
 // Chapter # 2 => Question # 9
@@ -73,19 +90,23 @@
 // alert(design);
 
 
+
 // Chapter # 3 => Question # 1
 // var age = 24;
 // alert("I am "+age+" years old");
+
 
 
 // Chapter # 3 => Question # 2
 // var visit = 1;
 
 
+
 // Chapter # 3 => Question # 3                                              //not done
 // var birthyear = 1995;
 // document.write("My Birthyear is "+birthyear+"<br>");
 // document.write("Date type of my declared variable is number");           
+
 
 
 // Chapter # 3 => Question # 4
@@ -99,8 +120,10 @@
 // document.write(visitor_name+" ordered "+quantity+" "+product_title+"(s) on XYZ Clothing Store.");
 
 
+
 // Chapter # 4 => Question # 1
 // var number, age, integer;
+
 
 
 // Chapter # 4 => Question # 2
@@ -117,6 +140,7 @@
 // var &year;
 
 
+
 // Chapter # 4 => Question # 3
 // document.write(`<h1 style="font-weight: bold">Rules for naming JS variables</h1> <br><br><br>`);
 // document.write(`Variable name can only contain, number, $ and _. For example: $my_1stVariable <br>`);
@@ -125,11 +149,13 @@
 // document.write(`Variable names should not be JS keywords <br>`);
 
 
+
 // Chapter # 5 => Question # 1
 // var num1 = parseInt(prompt());
 // var num2 = parseInt(prompt());
 // var add = num1+num2;
 // document.write(`Sum of ${num1} and ${num2} is ${add}`);
+
 
 
 // Chapter # 5 => Question # 2
@@ -141,6 +167,7 @@
 // document.write(`Division of ${num1} and ${num2} is ${divide}`);
 // var modulus = num1%num2;
 // document.write(`Remainder of ${num1} and ${num2} is ${modulus}`);
+
 
 
 // Chapter # 5 => Question # 3
@@ -158,6 +185,7 @@
 // document.write("The Remainder is: "+num+" <br> ");
 
 
+
 // Chapter # 5 => Question # 4
 // var movie_ticket = 600;
 // var num = parseInt(prompt());
@@ -165,6 +193,7 @@
 // total = total.toString();
 // total = total.bold();
 // document.write(`Total cost of ${num} tickets to a movie is ${total}<b>PKR</b>`)
+
 
 
 // Chapter # 5 => Question # 5
@@ -175,6 +204,7 @@
 // }
 
 
+
 // Chapter # 5 => Question # 6
 // var c = parseInt(prompt());
 // var fahrenheit = (c*9/5)+32;
@@ -183,6 +213,7 @@
 // var f = parseInt(prompt());
 // var celsius = (f-32)*5/9;
 // document.write(`${f}'C is ${celsius}'F`);
+
 
 
 // Chapter # 5 => Question # 7
@@ -202,6 +233,7 @@
 // document.write(`Total cost of your order is ${total}<br>`);
 
 
+
 // Chapter # 5 => Question # 8
 // var totalMarks= 980;
 // var marksObtained = 804;
@@ -213,6 +245,7 @@
 // document.write(`Percentage: ${percentage}%`);
 
 
+
 // Chapter # 5 => Question # 9
 // var USdollar = 10;
 // var saudiRiyals = 25;
@@ -222,10 +255,12 @@
 // document.write(`Total Currency in PKR: ${currency}`)
 
 
+
 // Chapter # 5 => Question # 10
 // var num = parseInt(prompt());
 // var calculation = ((num + 5) * 10) / 2;
 // document.write(calculation);
+
 
 
 // Chapter # 5 => Question # 11
@@ -241,6 +276,7 @@
 // document.write(`Your Age is : ${age2} or ${age}<br>`);
 
 
+
 // Chapter # 5 => Question # 12
 // var radius = parseInt(prompt());
 // var circumference = 2 * 3.142 * radius;
@@ -250,6 +286,7 @@
 // document.write(`Radius of a circle: ${radius} <br>`);
 // document.write(`The circumference is: ${circumference} <br>`);
 // document.write(`The area is: ${area} <br>`);
+
 
 
 // Chapter # 5 => Question # 13
@@ -267,6 +304,7 @@
 // document.write(`You will need ${totalquantity} ${favSnack} to last you until the ripe old age of ${estimatedAge}<br>`);
 
 
+
 // Chapter # 6-9 => Question # 1
 // var a = parseInt(prompt("Type a number"));
 // document.write(`<b>Result:</b> <br>`)
@@ -282,6 +320,7 @@
 // document.write(`Now the value of a is: ${a} <br><br>`)
 
 
+
 // Chapter # 6-9 => Question # 2
 // var a = 2, b = 1;
 // var result = (((--a) - (--b)) + (++b)) + (b--);
@@ -290,10 +329,12 @@
 // document.write(`result is ${result} <br>`);
 
 
+
 // Chapter # 6-9 => Question # 3
 // var name = prompt("Type your name:");
 // alert(`We welcome you ${name}`);
 // document.write(`<h1 style="font-weight: bold"> We welcome you ${name} </h1>`);
+
 
 
 // Chapter # 6-9 => Question # 4,5                                         // not done
@@ -310,6 +351,7 @@
 // }
 
 
+
 // Chapter # 6-9 => Question # 6                                           // not done
 // var sub1 = prompt("1st Subject:");
 // var sub2 = prompt("2nd Subject:");
@@ -318,6 +360,7 @@
 // var obtained1 = parseInt(prompt(`Marks obtained in ${sub1}:`))
 // var obtained2 = parseInt(prompt(`Marks obtained in ${sub2}:`))
 // var obtained3 = parseInt(prompt(`Marks obtained in ${sub3}:`))
+
 
 
 // Chapter # 9-11 => Question # 1                                          //case sensitive issue
@@ -336,6 +379,7 @@
 // }
 
 
+
 // Chapter # 9-11 => Question # 2
 // var gender = prompt("Type your Gender:");
 // if(gender === "male"){
@@ -346,15 +390,17 @@
 // }
 
 
-// Chapter # 9-11 => Question # 4                                           //logic confusion
-// var fuel = parseInt(prompt("Remaining Fuel in Litres"));
+
+// Chapter # 9-11 => Question # 4                                           
+// var fuel = parseFloat(prompt("Remaining Fuel in Litres"));
 // if(fuel > 0.25){
-    
+//     document.write(`Fuel is enough`);
 // } 
 // else{
 //     alert(`Please refill the fuel in your car`);
 //     document.write(`Please refill the fuel in your car`);
 // }
+
 
 
 // Chapter # 9-11 => Question # 6  
@@ -386,3 +432,411 @@
 // }
 
 
+
+// Chapter # 9-11 => Question # 7                                                               //one condition missing
+//  var numberGenerator = Math.floor(Math.random() * 10) + 1;
+//  var secretNumber = parseInt(prompt("Type a number between 1 to 10:"));
+//  document.write(`Generated Number: ${numberGenerator} <br>`);
+//  if(secretNumber >= 0 && secretNumber <= 10)
+//  {
+//     document.write(`Secret Number: ${secretNumber} <br>`);
+//     var newNumber = secretNumber;
+//  }
+//  else{
+//     document.write(`Secret Number: ${secretNumber} <br>`);
+//     document.write(`Secret Number is not between 1 to 10 <br>`);
+//  }
+ 
+//  if(numberGenerator === newNumber){
+//     document.write(`<b>Bingo! Correct answer</b>`);
+//  }
+//  else if(numberGenerator === (secretNumber+1)){
+//     document.write(`Close enough to correct answer`);
+//  }
+//  else if(numberGenerator === (secretNumber-1)){
+//     document.write(`Close enough to correct answer`);
+//  }
+//  else if(numberGenerator ){
+//     document.write(`Number is not between 1 to 10`);
+//  }
+
+
+
+// Chapter # 9-11 => Question # 8
+// var number = parseInt(prompt("Type an integer:"));
+// if(number%3==0){
+//         document.write(`${number} is divisible by 3`);
+// }
+// else{
+//     document.write(`${number} is not divisible by 3`);
+// }
+
+
+
+// Chapter # 9-11 => Question # 9
+// var number = parseInt(prompt("Type an integer:"));
+// if(number%2==0){
+//         document.write(`${number} is an even number`);
+// }
+// else{
+//     document.write(`${number} is an odd number`);
+// }
+
+
+
+// Chapter # 9-11 => Question # 10
+// var temperature = parseInt(prompt("Type a Temperature Degree:"));
+// if(temperature>40){
+//         document.write(`It's too hot outside.`);
+// }
+// else if(temperature>30){
+//     document.write(`The weather today is normal.`);
+// }
+// else if(temperature>20){
+//     document.write(`Today's weather is cool.`);
+// }
+// else if(temperature>10){
+//     document.write(`OMG! Today's weather is so cool`);
+// }
+
+
+
+// Chapter # 9-11 => Question # 11
+// var num1 = parseInt(prompt("Enter the first number:"));
+// var num2 = parseInt(prompt("Enter the second number:"));
+// var operator = prompt("Enter the operator:");
+// if(operator === "+"){
+//     document.write(`${num1+num2}`);
+// }
+// else if(operator === "-"){
+//     document.write(`${num1-num2}`);
+// }
+// else if(operator === "x" || operator === "*"){
+//     document.write(`${num1*num2}`);
+// }
+// else if(operator === "/"){
+//     document.write(`${num1/num2}`);
+// }
+
+
+
+// Chapter # 12-13 => Question # 1
+// var input = prompt();
+// var index = 0;
+// var i = input.charCodeAt(index);
+
+// if(i >= 65 && i <= 90){
+//     document.write(`This is capital ${input}.`)
+// }
+// else if(i >= 97 && i <= 122){
+//     document.write(`This is small ${input}.`)
+// }
+// else if(i >= 48 && i <= 57){
+//     document.write(`It's a number ${input}.`);
+// }
+// else{
+//     document.write(`${input} is a special character.`)
+// }
+
+
+
+// Chapter # 12-13 => Question # 2
+// var num1 = parseInt(prompt("1st Integer"))
+// var num2 = parseInt(prompt("2nd Integer"))
+
+// if(num1>num2){
+//     document.write(`${num1} is larger`);
+// }
+// else if(num2>num1){
+//     document.write(`${num2} is larger`);
+// }
+// else
+// if(num1 == num2){
+//     document.write(`${num1} & ${num2} both are equal.`);
+// }
+
+
+
+// Chapter # 12-13 => Question # 3
+// var num = parseInt(prompt());
+// if(num>0){
+//     document.write(`The number is positive`);
+// }
+// else if(num<0){
+//     document.write(`The number is negative`);
+// }
+// else if(num == 0){
+//     document.write(`The number is Zero`);
+// }
+
+
+
+// Chapter # 12-13 => Question # 4
+// var character = prompt();
+// if(character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u')
+// {
+//     document.write("It is a vowel");
+// }
+// else if(character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U')
+// {
+//     document.write("It is a vowel");
+// }
+// else{
+//     document.write("It is a consonant");
+// }
+
+
+
+// Chapter # 12-13 => Question # 5
+// var savedpassword = "shiraz123";
+// var password = prompt("Type a password");
+// var index = 0;
+// var i = password.charCodeAt(index);
+
+// if(password == "")
+// {
+//     document.write("Please enter your password");
+// } 
+// else{
+//     if(savedpassword === password){
+//         document.write("Correct! The password you entered matches the original password.");
+//     }
+//     else{
+//         document.write("Incorrect password");
+//     }
+// }
+
+
+
+// Chapter # 12-13 => Question # 6
+// var greeting;
+// var hour = 13;
+// if(hour < 18){
+//     greeting = "Good day";
+// }
+// else{
+//     greeting = "Good evening";    
+// }
+// document.write(greeting);
+
+
+
+// Chapter # 12-13 => Question # 7
+// var time = parseInt(prompt("Type the time in 24 Hours Clock Format:"));
+// if(time >0 && time<12)
+// {
+//     document.write(`The time is ${time}am.`)
+// }
+// else if(time>12 && time <24){
+//     time = time-12;
+//     document.write(`The time is ${time}pm.`)
+// }
+// else if (time == 24){
+//     time = time-12;
+//     document.write(`The time is ${time}midnight.`)
+// }
+// else if (time == 12){
+//     document.write(`The time is ${time}noon.`)
+// }
+
+
+
+// Chapter # 14-16 => Question # 1
+// var arrayLiteral = [];
+
+
+
+// Chapter # 14-16 => Question # 2
+// var arrayObject = new Array();
+
+
+
+// Chapter # 14-16 => Question # 3
+// var arrayString = ["Shiraz", "Usman", "Shoaib", "Aman", "Asad", "Khalid", "Asim"];
+
+
+
+// Chapter # 14-16 => Question # 4
+// var arrayNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+
+// Chapter # 14-16 => Question # 5                                                          //not done // boolean array initialize 
+// var array = 
+
+
+
+// Chapter # 14-16 => Question # 6
+// var array = ["Shiraz", 12, true, 2.5, 'A' ];
+
+
+
+// Chapter # 14-16 => Question # 7
+// var qualificationArray = ["SSC", "HSC", "BSC", "BS", "BCOM", "MS", "MPhil", "PhD"];
+// document.write(`<b>Qualifications:</b><br><br>`)
+// for (let i = 0; i < qualificationArray.length; i++) {
+//     document.write(`${i+1}) ${qualificationArray[i]} <br>`);
+// }
+
+
+
+// Chapter # 14-16 => Question # 8
+// var studentArray = ["Shiraz","Yasir", "Shoaib"];
+// var marksArray = [320, 230, 480];
+
+// for(i=0; i<studentArray.length; i++){
+//     document.write(`Score of ${studentArray[i]} is ${marksArray[i]}. Percentage: ${(marksArray[i]/500)*100}% <br>`);
+// }
+
+
+
+// Chapter # 14-16 => Question # 9                                                          // not done
+// var colorArray = ["Black", "Blue", "Orange", "Green", "Red", "Yellow", "Pink", "Purple", "Brown"];
+
+
+
+// Chapter # 14-16 => Question # 10
+// var studentScores = [320, 230, 480, 120];
+// document.write(`Scores of Students: ${studentScores}<br>`);
+// studentScores.sort();
+// document.write(`Sorted Scores of Students: ${studentScores}`);
+
+
+
+// Chapter # 14-16 => Question # 11                                                            //not done
+// var city_array = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"]
+
+
+
+// Chapter # 14-16 => Question # 12
+// var arr = ["This", "is", "my", "cat"];
+// document.write(`Array: <br>`);
+// document.write(`${arr} <br><br>`)
+// document.write(`String: <br>`);
+// for(i=0; i<arr.length; i++){
+//     document.write(`${arr[i]} `);
+// }
+//              (OR)
+// var arr1 = arr.join(" ");
+// document.write("<br>");
+// document.write("<br>");
+// document.write("String:");
+// document.write("<br>");
+// document.write(arr1);
+
+
+
+// Chapter # 14-16 => Question # 13                                         //need to use push or pop condition
+// var devices_array = ["keyboard", "mouse", "printer", "monitor"];
+// document.write(`Devices: <br>`);
+// document.write(`${devices_array} <br><br>`);
+
+// for(i=0; i<devices_array.length; i++)
+// {
+//     document.write(`Out: <br>`);
+//     document.write(`${devices_array[i]} <br>`);
+// }
+
+
+
+// Chapter # 14-16 => Question # 14                                          //need to use push or pop condition
+// var devices_array = ["keyboard", "mouse", "printer", "monitor"];
+// document.write(`Devices: <br>`);
+// document.write(`${devices_array} <br><br>`);
+
+// for(i=devices_array.length-1; i>=0; i--)
+// {
+//     document.write(`Out: <br>`);
+//     document.write(`${devices_array[i]} <br>`);
+// }
+
+
+
+// Chapter # 14-16 => Question # 14                                            // not done (don't know how to create dropdown/Select menu using document.write method)
+// var mobile_array;
+// mobile_array.push("Apple","Samsung","Motorolla","Nokia", "Sony", "Haier");
+
+
+
+// Chapter # 17-20 => Question # 1                                              //not done
+// var array = [[],[]];
+
+
+
+// Chapter # 17-20 => Question # 2                                              //not done
+
+
+
+// Chapter # 17-20 => Question # 3
+// for(i=1; i<=10; i++)
+// {
+//     document.write(`${i}<br>`);
+// }
+
+
+
+// Chapter # 17-20 => Question # 4
+// var _table = parseInt(prompt("Enter a number to show its multiplication:"));
+// var _length = parseInt(prompt("Enter length of multiplication table:"));
+// for(i = 1; i <= _length; i++)
+// {
+//     document.write(`${_table} x ${i} = ${_table*i}<br>`);
+// }
+
+
+
+// Chapter # 17-20 => Question # 5
+// var fruits_array =  [];
+// fruits_array.push("apple", "banana", "mango", "orange", "strawberry");
+
+// for(i = 0; i < fruits_array.length; i++)
+// {
+//     document.write(`${fruits_array[i]} <br>`);
+// }
+// for(i = 0; i < fruits_array.length; i++)
+// {
+//     document.write(`Element at index ${i} is ${fruits_array[i]} <br>`);
+// }
+
+
+
+// Chapter # 17-20 => Question # 6(a)
+document.write(`<b>Counting:</b> <br><br>`);
+for(var i=1;i<=15; i++) {
+    document.write(`${i}, `)
+}
+
+
+
+// Chapter # 17-20 => Question # 6(b)
+// document.write(`<br><br> <b>Reverse:</b> <br><br>`);
+// for(var i=10; i>=1; i--) {
+//     document.write(`${i}, `)
+// }
+
+
+
+// Chapter # 17-20 => Question # 6(c)
+// document.write(`<br><br> <b>Odd:</b> <br><br>`);
+// for(var i=0; i<=20; ) {
+//     document.write(`${i}, `);
+//     i = i+2;
+// }
+
+
+
+// Chapter # 17-20 => Question # 6(d)
+// document.write(`<br><br> <b>Even:</b> <br><br>`);
+// for(var i=1; i<=20; ) {
+//     document.write(`${i}, `);
+//     i = i+2;
+// }
+
+
+
+// Chapter # 17-20 => Question # 6(e)
+// document.write(`<br><br> <b>Series:</b> <br><br>`);
+// for(var i=2; i<=20; ) {
+//     document.write(`${i}k, `);
+//     i = i+2;
+// }
